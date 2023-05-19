18111b8e26d6275d42bdbd81d1a1daf55abfd950
cronix <cronix0325@163.com>
2023-05-19 21:52:08 +0800

    update all datatype to double

--- raytracing.cpp
+++ raytracing.cpp
@@ -11,42 +11,42 @@
 #include "algebra/algebra.hpp"
 #include "stb/stb_image_write.hpp"
 
-const float infinity = std::numeric_limits<float>::infinity();
+const double infinity = std::numeric_limits<double>::infinity();
 
 
 class ray {
 
 public:
     ray () {}
-    ray (vec3f orig, vec3f dir) 
+    ray (vec3d orig, vec3d dir) 
     {
         this->orig = orig;
         this->dir = dir;
     }
 
-    vec3f origin() const { return orig; }
+    vec3d origin() const { return orig; }
 
-    vec3f direction() const { return dir; }
+    vec3d direction() const { return dir; }
 
-    vec3f at(float t) const { return orig + t * dir; }
+    vec3d at(double t) const { return orig + t * dir; }
 
 private:
-    vec3f orig;
-    vec3f dir;
+    vec3d orig;
+    vec3d dir;
 };
 
 
 class intersection {
 
 public:
-    vec3f point;
-    vec3f normal;
-    float t;
+    vec3d point;
+    vec3d normal;
+    double t;
     bool frontward;
 
     intersection() {}
 
-    void set_face_normal(const ray& r, const vec3f& outward_normal)
+    void set_face_normal(const ray& r, const vec3d& outward_normal)
     {
         frontward = r.direction().dot(outward_normal) < 0;
         normal = frontward ? outward_normal : -outward_normal;
@@ -57,37 +57,37 @@ public:
 class object {
 
 public:
-    virtual bool intersect(const ray& r, float t_min, float t_max, intersection& crossover) const = 0;
+    virtual bool intersect(const ray& r, double t_min, double t_max, intersection& crossover) const = 0;
 };
 
 
 class sphere : public object {
 
 public:
-    vec3f center;
-    float radius;
+    vec3d center;
+    double radius;
 
     sphere() {}
 
-    sphere(vec3f center, float radius) 
+    sphere(vec3d center, double radius) 
     {
         this->center = center;
         this->radius = radius;
     }
 
-    virtual bool intersect(const ray& r, float t_min, float t_max, intersection& crossover) const override
+    virtual bool intersect(const ray& r, double t_min, double t_max, intersection& crossover) const override
     {
-        vec3f oc = r.origin() - center;
-        float a = r.direction().dot(r.direction());
-        float b = 2.0 * oc.dot(r.direction());
-        float c = oc.dot(oc) - radius * radius;
-        float discriminant = b * b - 4 * a * c;
+        vec3d oc = r.origin() - center;
+        double a = r.direction().dot(r.direction());
+        double b = 2.0 * oc.dot(r.direction());
+        double c = oc.dot(oc) - radius * radius;
+        double discriminant = b * b - 4 * a * c;
 
         if (discriminant < 0) {
             return false;
         }
 
-        float root = (-b - sqrt(discriminant)) / (a * 2);
+        double root = (-b - sqrt(discriminant)) / (a * 2);
         
         if (root < t_min || root > t_max) {
             root = (-b + sqrt(discriminant)) / (a * 2);
@@ -97,7 +97,7 @@ public:
         }
         crossover.t = root;
         crossover.point = r.at(root);
-        vec3f outward_normal = (crossover.point - center) / radius;
+        vec3d outward_normal = (crossover.point - center) / radius;
         crossover.set_face_normal(r, outward_normal);
         return true;
     }
@@ -117,11 +117,11 @@ public:
 
     void clear() { objects.clear(); }
 
-    virtual bool intersect(const ray& r, float t_min, float t_max, intersection& crossover) const override
+    virtual bool intersect(const ray& r, double t_min, double t_max, intersection& crossover) const override
     {
         intersection temp_crossover;
         bool has_intersect = false;
-        float closest = t_max;
+        double closest = t_max;
 
         for (auto& object : objects) {
             if (object->intersect(r, t_min, closest, temp_crossover)) {
@@ -138,66 +138,66 @@ public:
 class camera {
 
 public:
-    vec3f origin;
-    float sensor_w;
-    float sensor_h;
-    float focal_length;
+    vec3d origin;
+    double sensor_w;
+    double sensor_h;
+    double focal_length;
 
     camera() {}
 
-    camera(vec3f origin, float sensor_w, float sensor_h, float focal_length)
+    camera(vec3d origin, double sensor_w, double sensor_h, double focal_length)
     {
         this->sensor_w = sensor_w;
         this->sensor_h = sensor_h;
         this->focal_length = focal_length;
 
-        horizontal = vec3f(sensor_w, 0, 0);
-        vertical = vec3f(0, sensor_h, 0);
-        left_bottom = origin - horizontal / 2 - vertical / 2 - vec3f(0, 0, focal_length);
+        horizontal = vec3d(sensor_w, 0, 0);
+        vertical = vec3d(0, sensor_h, 0);
+        left_bottom = origin - horizontal / 2 - vertical / 2 - vec3d(0, 0, focal_length);
     }
 
-    ray emit(float x, float y) const
+    ray emit(double x, double y) const
     {
-        vec3f direction = left_bottom + x * horizontal + y * vertical - origin;
+        vec3d direction = left_bottom + x * horizontal + y * vertical - origin;
         return ray(origin, direction);
     }
 
 private:
-    vec3f left_bottom;
-    vec3f horizontal;
-    vec3f vertical;
+    vec3d left_bottom;
+    vec3d horizontal;
+    vec3d vertical;
 };
 
 
-float random()
+double random()
 {
-    return rand() / float(RAND_MAX);
+    return rand() / double(RAND_MAX);
 }
 
 
-float random(float min, float max)
+double random(double min, double max)
 {
     return min + (max - min) * random();
 }
 
 
-vec3f random_vector()
+vec3d random_vector()
 {
-    return vec3f(random(), random(), random());
+    return vec3d(random(), random(), random());
 }
 
 
-vec3f random_vector(float min, float max)
+vec3d random_vector(double min, double max)
 {
-    return vec3f(random(min, max), random(min, max), random(min, max));
+    return vec3d(random(min, max), random(min, max), random(min, max));
 }
 
 
-vec3f random_shpere()
+vec3d random_shpere()
 {
     while (true) {
-        vec3f point = random_vector(-1, 1);
-        float length = point.length();
+        vec3d point = random_vector(-1, 1);
+        double length = point.length();
         if (length * length >= 1) {
             continue;
         }
@@ -206,16 +206,16 @@ vec3f random_shpere()
 }
 
 
-vec3f gamma(vec3f v, float power)
+vec3d gamma(vec3d value, double power)
 {
-    float x = pow(v.x(), power);
-    float y = pow(v.y(), power);
-    float z = pow(v.z(), power);
-    return vec3f(x, y, z);
+    double x = pow(value.x(), power);
+    double y = pow(value.y(), power);
+    double z = pow(value.z(), power);
+    return vec3d(x, y, z);
 }
 
 
-float clamp(float value, float min, float max)
+double clamp(double value, double min, double max)
 {
     if (value < min) {
         return min;
@@ -229,29 +229,38 @@ float clamp(float value, float min, float max)
 }
 
 
-vec3f shading_ray(const scene& scn, const ray& r, int depth)
+vec3d clamp(vec3d value, double min, double max)
+{
+    double x = clamp(value.x(), min, max);
+    double y = clamp(value.y(), min, max);
+    double z = clamp(value.z(), min, max);
+    return vec3d(x, y, z);
+}
+
+
+vec3d shading_ray(const scene& scn, const ray& r, int depth)
 {
     if (depth <= 0) {
-        return vec3f(0, 0, 0);
+        return vec3d(0, 0, 0);
     }
 
     intersection crossover;
-    if (scn.intersect(r, 0, infinity, crossover)) {
-        vec3f next = crossover.point + crossover.normal + random_shpere();
+    if (scn.intersect(r, 0.01, infinity, crossover)) {
+        vec3d next = crossover.point + crossover.normal + random_shpere();
         ray next_r =  ray(crossover.point, next - crossover.point);
         return 0.5 * shading_ray(scn, next_r, depth - 1);
     }
-    float t = 0.5 * (r.direction().normalize().y() + 1.0);
-    return (1.0 - t) * vec3f(1.0, 1.0, 1.0) + t * vec3f(0.5, 0.7, 1.0);
+    double t = 0.5 * (r.direction().normalize().y() + 1.0);
+    return (1.0 - t) * vec3d(1.0, 1.0, 1.0) + t * vec3d(0.5, 0.7, 1.0);
 }
 
 
 void render_image(const char* path, int width, int height)
 {
     scene scn;
-    scn.add(std::make_shared<sphere>(sphere(vec3f(0, 0, -1), 0.5)));
-    scn.add(std::make_shared<sphere>(sphere(vec3f(0, -100.5, -1), 100)));
-    camera cam = camera(vec3f(0, 0, 0), 3.56, 2.0, 1.0);
+    scn.add(std::make_shared<sphere>(sphere(vec3d(0, 0, -1), 0.5)));
+    scn.add(std::make_shared<sphere>(sphere(vec3d(0, -100.5, -1), 100)));
+    camera cam = camera(vec3d(0, 0, 0), 3.56, 2.0, 1.0);
 
     int spp = 100;
     int max_depth = 10;
@@ -263,14 +272,15 @@ void render_image(const char* path, int width, int height)
     for (int i = 0; i < height; i++) {
         printf("\rRendering (%d spp) %5.2f%%", spp, 100. * i / (height - 1));
         for (int j = 0; j < width; j++) {
-            vec3f color = vec3f(0, 0, 0);
+            vec3d color = vec3d(0, 0, 0);
             for (int k = 0; k < spp; k++) {
-                float x = float(j + random()) / (width - 1);
-                float y = float(i + random()) / (height - 1);
+                double x = double(j + random()) / (width - 1);
+                double y = double(i + random()) / (height - 1);
                 ray r = cam.emit(x, y);
                 color += shading_ray(scn, r, max_depth);
             }
             color /= spp;
+            color = clamp(color, 0, 1);
             color = gamma(color, 0.45);
             int index = (i * width + j) * 3;
             data[index] = int(color.x() * 255);
