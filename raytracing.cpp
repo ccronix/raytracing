b9e40e034ebbc78ff977e58b0904d06c6f3246bf
cronix <cronix0325@163.com>
2023-05-21 17:55:09 +0800

    implement bounding volume hierarchy

--- raytracing.cpp
+++ raytracing.cpp
@@ -4,6 +4,7 @@
 #include <omp.h>
 #include <vector>
 #include <limits>
+#include <algorithm>
 
 #include <ctime>
 #include <cstdio>
@@ -38,6 +39,12 @@ double random()
 double random(double min, double max)
 {
     return min + (max - min) * random();
+}
+
+
+int random_int(int min, int max)
+{
+    return int(random(min, max + 1));
 }
 
 
@@ -508,6 +515,96 @@ public:
 };
 
 
+class bvh : public object{
+
+public:
+    object* left;
+    object* right;
+    aabb bbox;
+
+    bvh() {}
+
+    bvh(const scene& scn, double start, double end) : bvh(scn.objects, 0, scn.objects.size(), start, end) {}
+
+    bvh(const std::vector<object*>& sources, int first, int last, double start, double end)
+    {
+        std::vector<object*> objects = sources;
+        int axis = random_int(0, 2);
+        auto function = (axis == 0) ? comparex : (axis == 1) ? comparey : comparez;
+        int span = last - first;
+
+        if (span == 1) {
+            right = objects[first];
+            left = right;
+        }
+        else if (span == 2) {
+            if (function(objects[first], objects[first + 1])) {
+                left = objects[first];
+                right = objects[first + 1];
+            }
+            else {
+                left = objects[first + 1];
+                right = objects[first];
+            }
+        }
+        else {
+            std::sort(objects.begin() + first, objects.begin() + last, function);
+            int middle = first + span / 2;
+            left = new bvh(objects, first, middle, start, end);
+            right = new bvh(objects, middle, last, start, end);
+        }
+
+        aabb bbox_left, bbox_right;
+        if (!left->bounding_box(start, end, bbox_left) || !right->bounding_box(start, end, bbox_right)) {
+            fprintf(stderr, "[ERROR] no bounding box in bvh constructor.");
+        }
+        bbox = surrounding_box(bbox_left, bbox_right);
+    }
+
+    virtual bool intersect(const ray& r, double t_min, double t_max, intersection& crossover) const override
+    {
+        if (!bbox.intersect(r, t_min, t_max)) {
+            return false;
+        }
+
+        bool hit_left = left->intersect(r, t_min, t_max, crossover);
+        double t_max_right = hit_left ? crossover.t : t_max;
+        bool hit_right = right->intersect(r, t_min, t_max_right, crossover);
+        return hit_left || hit_right;
+    }
+
+    virtual bool bounding_box(double start, double end, aabb& output) const override
+    {
+        output = bbox;
+        return true;
+    }
+
+    static bool compare(const object* a, const object* b, int axis)
+    {
+        aabb bbox_a, bbox_b;
+        if (!a->bounding_box(0, 0, bbox_a) || !b->bounding_box(0, 0, bbox_b)) {
+            fprintf(stderr, "[ERROR] no bounding box in bvh constructor.");
+        }
+        return bbox_a.min()[axis] < bbox_b.min()[axis];
+    }
+
+    static bool comparex(const object* a, const object* b)
+    {
+        return compare(a, b, 0);
+    }
+
+    static bool comparey(const object* a, const object* b)
+    {
+        return compare(a, b, 1);
+    }
+
+    static bool comparez(const object* a, const object* b)
+    {
+        return compare(a, b, 2);
+    }
+};
+
+
 class camera {
 
 public:
@@ -638,7 +735,9 @@ scene random_scene()
 
     material* gold = new metal(vec3d(0.7, 0.6, 0.5), 0);
     scn.add(new sphere(vec3d(4, 1, 0), 1.0, gold));
-    return scn;
+    bvh* bvh_scn = new bvh(scn, 0, 1);
+    return scene(bvh_scn);
+    // return scn;
 }
 
 
@@ -662,7 +761,7 @@ void render_image(const char* path, int width, int height)
     // camera cam = camera(vec3d(3, 3, 2), vec3d(0, 0, -1), vec3d(0, 1, 0), 45, 1.78, 2.0, 5.2);
     camera cam = camera(vec3d(13, 2, 3), vec3d(0, 0, 0), vec3d(0, 1, 0), 30, 1.78, 0.1, 10, 0, 1);
 
-    int spp = 10;
+    int spp = 100;
     int max_depth = 10;
 
     unsigned char* data = (unsigned char*) malloc(width * height * sizeof(unsigned char) * 3);
