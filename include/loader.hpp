c78a50adae9619fbd4cdc1b85d9d55ef38646259
cronix <cronix0325@163.com>
2023-05-29 22:23:00 +0800

    implement polygon obj mesh load

---
+++ include/loader.hpp
@@ -0,0 +1,144 @@
+#define TINYOBJLOADER_IMPLEMENTATION
+
+
+#include <cstdio>
+#include <vector>
+#include <string>
+
+#include "scene.hpp"
+#include "polygon.hpp"
+#include "texture.hpp"
+#include "material.hpp"
+#include "algebra/algebra.hpp"
+#include "loader/tiny_obj_loader.hpp"
+
+
+class loader {
+
+public:
+    loader(const char* path)
+    {
+        scene_path = path;
+        tinyobj::ObjReader reader;
+        tinyobj::ObjReaderConfig config;
+        config.mtl_search_path = dirname(path).c_str();
+
+        if (!reader.ParseFromFile(path, config)) {
+            printf("[TinyOBJLoader] error: %s\n", reader.Error().c_str());
+            return;
+        }
+
+        if (!reader.Warning().empty()) {
+            printf("[TinyOBJLoader] warning: %s\n", reader.Warning().c_str());
+        }
+
+        load_material(reader);
+        load_mesh(reader);
+    }
+
+    scene meshes() { return scn; }
+
+
+private:
+    scene scn;
+    std::string scene_path;
+    std::vector<material*> mats;
+
+    std::string dirname(const std::string& path) const
+    {
+        std::string::size_type slash_index = path.find_last_of("/\\");
+        std::string scene_dir;
+
+        if (slash_index == std::string::npos) {
+            scene_dir = ".";
+        }
+        else if (slash_index == 0) {
+            scene_dir = "/";
+        }
+        else {
+            scene_dir = path.substr(0, slash_index);
+        }
+        return scene_dir;
+    }
+
+    void load_mesh(tinyobj::ObjReader reader)
+    {
+        tinyobj::attrib_t attrib = reader.GetAttrib();
+        std::vector<tinyobj::shape_t> shapes = reader.GetShapes();
+
+        for (auto& shape : shapes) {
+            printf("[TinyOBJLoader] load shape: %s\n", shape.name.c_str());
+            int mat_id;
+            int offset = 0;
+            for (int face_id = 0; face_id < shape.mesh.num_face_vertices.size(); face_id++) {
+                int face_num_vertices = shape.mesh.num_face_vertices[face_id];
+
+                if (face_num_vertices != 3) {
+                    continue;
+                }
+
+                std::vector<vertex> vertices(3);
+                int mat_id = shape.mesh.material_ids[face_id];
+
+                for (int vertex_id = 0; vertex_id < face_num_vertices; vertex_id++) {
+                    tinyobj::index_t index = shape.mesh.indices[offset + vertex_id];
+
+                    float px = attrib.vertices[3 * index.vertex_index];
+                    float py = attrib.vertices[3 * index.vertex_index + 1];
+                    float pz = attrib.vertices[3 * index.vertex_index + 2];
+
+                    vertices[vertex_id].position = vec3d(px, py, pz);
+
+                    if (index.normal_index >= 0) {
+                        float nx = attrib.normals[3 * index.normal_index];
+                        float ny = attrib.normals[3 * index.normal_index + 1];
+                        float nz = attrib.normals[3 * index.normal_index + 2];
+
+                        vertices[vertex_id].normal = vec3d(nx, ny, nz);
+                    }
+                    else {
+                        vec3d p0p1 = vertices[1].position - vertices[0].position;
+                        vec3d p0p2 = vertices[2].position - vertices[0].position;
+                        vec3d normal = p0p1.cross(p0p2).normalize();
+
+                        vertices[vertex_id].normal = normal;
+                    }
+
+                    if (index.texcoord_index >= 0) {
+                        float u = attrib.texcoords[2 * index.texcoord_index];
+                        float v = attrib.texcoords[2 * index.texcoord_index + 1];
+
+                        vertices[vertex_id].uv_coord = vec2d(u, v);
+                    }
+                    else {
+                        vertices[vertex_id].uv_coord = vec2d(0, 0);
+                    }
+                }
+                offset += face_num_vertices;
+                triangle* mesh_triangle = new triangle(vertices, mats[mat_id]);
+                scn.add(mesh_triangle);
+            }
+        }
+    }
+
+    void load_material(tinyobj::ObjReader reader)
+    {
+        texture* base_color;
+        std::vector<tinyobj::material_t> materials = reader.GetMaterials();
+        std::string scene_dir = dirname(scene_path);
+
+        for (auto& obj_mat : materials) {
+            if (!obj_mat.diffuse_texname.empty()) {
+                std::string image_path = scene_dir + "/" + obj_mat.diffuse_texname;
+                printf("[TinyOBJLoader] load diffuse image: %s\n", image_path.c_str());
+                base_color = new image(image_path.c_str());
+            }
+            else {
+                vec3d color = vec3d(obj_mat.diffuse[0], obj_mat.diffuse[1], obj_mat.diffuse[2]);
+                printf("[TinyOBJLoader] load constant: vec3(%f, %f, %f)\n", color.x(), color.y(), color.z());
+                base_color = new constant(color);
+            }
+            mats.push_back(new lambertian(base_color));
+        }
+    }
+};
---
+++ include/polygon.hpp
@@ -0,0 +1,170 @@
+#pragma once
+
+
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+#include "ray.hpp"
+#include "util.hpp"
+#include "object.hpp"
+#include "material.hpp"
+#include "intersection.hpp"
+#include "algebra/algebra.hpp"
+
+
+struct vertex
+{
+    vec3d position;
+    vec2d uv_coord;
+    vec3d normal;
+};
+
+
+class triangle : public object {
+
+public:
+    triangle(std::vector<vertex> vertices, material* mat) 
+    {
+        for (int i = 0; i < 3; i++) {
+            this->vertices.push_back(vertices[i]);
+        }
+
+        p0p1 = vertices[1].position - vertices[0].position;
+        p0p2 = vertices[2].position - vertices[0].position;
+
+        this->mat = mat;
+    }
+
+    triangle(vertex v0, vertex v1, vertex v2, material* mat)
+    {
+        vertices.push_back(v0);
+        vertices.push_back(v1);
+        vertices.push_back(v2);
+
+        p0p1 = vertices[1].position - vertices[0].position;
+        p0p2 = vertices[2].position - vertices[0].position;
+
+        this->mat = mat;
+    }
+
+    triangle(vec3d p0, vec3d p1, vec3d p2, material* mat)
+    {
+        vertex v0, v1, v2;
+
+        v0.position = p0;
+        v1.position = p1;
+        v2.position = p2;
+
+        v0.uv_coord = vec2d(p0.x(), p0.y());
+        v1.uv_coord = vec2d(p1.x(), p1.y());
+        v2.uv_coord = vec2d(p2.x(), p2.y());
+
+        v0.normal = (p1 - p0).cross(p2 - p0).normalize();
+        v1.normal = (p2 - p1).cross(p0 - p1).normalize();
+        v2.normal = (p0 - p2).cross(p1 - p2).normalize();
+
+        vertices.push_back(v0);
+        vertices.push_back(v1);
+        vertices.push_back(v2);
+
+        p0p1 = vertices[1].position - vertices[0].position;
+        p0p2 = vertices[2].position - vertices[0].position;
+
+        this->mat = mat;
+    }
+
+    virtual bool intersect(const ray& r, intersection& crossover) const override
+    {
+
+        vec3d q = r.direction.cross(p0p2);
+        double a = p0p1.dot(q);
+        if (fabs(a) < epsilon) {
+            return false;
+        }
+
+        double f = 1.0 / a;
+        vec3d s = r.origin - vertices[0].position;
+        double u = f * s.dot(q);
+        if (u < 0) {
+            return false;
+        }
+
+        vec3d k = s.cross(p0p1);
+        double v = f * r.direction.dot(k);
+        if (v < 0 || u + v > 1) {
+            return false;
+        }
+
+        double t = f * p0p2.dot(k);
+        if (t < r.t_min || t > r.t_max) {
+            return false;
+        }
+
+        crossover.t = t;
+        crossover.position = r.at(t);
+        crossover.uv_coord = interpret_uv(u, v);
+        vec3d outward_normal = interpret_normal(u, v);
+        crossover.set_face_normal(r, outward_normal);
+        crossover.mat = mat;
+        return true;
+    }
+
+    virtual bool bounding_box(double start, double end, aabb& bbox) const override
+    {
+        double x_min = std::min({vertices[0].position.x(), vertices[1].position.x(), vertices[2].position.x()}) - epsilon;
+        double y_min = std::min({vertices[0].position.y(), vertices[1].position.y(), vertices[2].position.y()}) - epsilon;
+        double z_min = std::min({vertices[0].position.z(), vertices[1].position.z(), vertices[2].position.z()}) - epsilon;
+
+        double x_max = std::max({vertices[0].position.x(), vertices[1].position.x(), vertices[2].position.x()}) + epsilon;
+        double y_max = std::max({vertices[0].position.y(), vertices[1].position.y(), vertices[2].position.y()}) + epsilon;
+        double z_max = std::max({vertices[0].position.z(), vertices[1].position.z(), vertices[2].position.z()}) + epsilon;
+
+        bbox = aabb(vec3d(x_min, y_min, z_min), vec3d(x_max, y_max, z_max));
+        return true;
+    }
+
+    virtual double pdf(const vec3d& origin, const vec3d& value) const override
+    {
+        intersection crossover;
+        if (!this->intersect(ray(origin, value), crossover)) {
+            return 0;
+        }
+
+        double area = p0p1.cross(p0p2).length() / 2.0;
+        double distance_sqr = crossover.t * crossover.t * value.length() * value.length();
+        double cosine = fabs(value.dot(crossover.normal) / value.length());
+        return distance_sqr / (cosine * area);
+    }
+
+    virtual vec3d rand(const vec3d& origin) const override
+    {
+        vec3d point;
+        double u = random_double();
+        double v = random_double();
+
+        if (u + v > 1) {
+            point = vertices[0].position + (1 - u) * p0p1 + (1 - v) * p0p2;
+        }
+        else {
+            point = vertices[0].position + u * p0p1 + v * p0p2;
+        }
+
+        return point - origin;
+    }
+
+private:
+    vec3d p0p1, p0p2;
+    std::vector<vertex> vertices;
+    material* mat;
+
+    vec2d interpret_uv(double u, double v) const
+    {
+        return u * vertices[1].uv_coord + v * vertices[2].uv_coord + (1 - u - v) * vertices[0].uv_coord;
+    }
+
+    vec3d interpret_normal(double u, double v) const
+    {
+        return u * vertices[1].normal + v * vertices[2].normal + (1 - u - v) * vertices[0].normal;
+    }
+};
